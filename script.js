a5aacb9a0894393ec6955d4d60ac54c6adc1af09
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-12-04 16:41:18 +0100

    add: basic js code for carosel

--- script.js
+++ script.js
@@ -29,3 +29,66 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', 
     } 
 ];
+
+// selection of all the elemnts with item class
+const items = document.getElementsByClassName("item");
+//console.log(items);
+
+// selection of the buttons
+const up = document.querySelector(".up");
+//console.log(up);
+
+const down = document.querySelector(".down");
+//console.log(down);
+
+let activeItem = 0;
+
+//events on click up button
+up.addEventListener("click", function() {
+
+    if (activeItem < items.length - 1)  {
+
+        //delete class active to the element
+        items[activeItem].classList.remove("active");
+
+        activeItem++;
+
+        //add class active to the next element
+        items[activeItem].classList.add("active");
+
+    }
+
+     //delete up button
+     if (activeItem === items.length - 1)    {
+        up.classList.add("hidden");
+    }
+
+    //show down button
+    down.classList.remove("hidden");
+
+});
+
+// events on click down button
+down.addEventListener("click", function() {
+
+   if (activeItem > 0) {
+
+        //delete class active to the element
+        items[activeItem].classList.remove("active");
+
+        activeItem--;
+
+        //add class active to the next element
+        items[activeItem].classList.add("active");
+
+   }
+
+    //delete down button
+    if (activeItem === 0)   {
+        down.classList.add("hidden")
+    }
+
+    //shoow up button
+    up.classList.remove("hidden");
+
+});
